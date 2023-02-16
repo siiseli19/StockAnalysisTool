b7eb26dfd7eb39f7cc2729caca491b7f659d303e
Sami <sami.ojala97@gmail.com>
2023-02-17 00:29:27 +0200

    fixed data-reader problem and fetched FRED data

--- valuation.py
+++ valuation.py
@@ -1,3 +1,4 @@
+import datareader as datareader
 import requests
 import re
 import csv
@@ -5,52 +6,62 @@ import json
 from bs4 import BeautifulSoup
 from io import StringIO
 import pandas as pd
+import pandas_datareader.data as web
 import sys
-import numpy as np #for pv and fv functions
 import os
-
-
+import datetime
 
 
 # from financialmodelingprep API
-#Kd = Rf + Credit Spread
-#Risk free rate we will use the interest rate offered by a 1 year US T-bill >> scrape if needed
+# Kd = Rf + Credit Spread
+# Risk free rate we will use the interest rate offered by a 1 year US T-bill >> scrape if needed
 def interest_coverage_and_RF(ticker):
     stock_ticker = ticker
     key = os.environ.get('API_KEY')
 
-    IS = requests.get(f'https://financialmodelingprep.com/api/v3/income-statement/{stock_ticker}?apikey={key}').json() #income statement
-    EBIT = IS[0]['ebitda'] - IS[0]['depreciationAndAmortization'] #index 0 for the most recent year
+    # Interest coverage
+    IS = requests.get(
+        f'https://financialmodelingprep.com/api/v3/income-statement/{stock_ticker}?apikey={key}').json()  # income statement
+    EBIT = IS[0]['ebitda'] - IS[0]['depreciationAndAmortization']  # index 0 for the most recent year
     interest_expense = IS[0]['interestExpense']
     interest_coverage_ratio = EBIT / interest_expense
-    return interest_coverage_ratio
+
+    # RF
+    start = datetime.datetime(2019, 7, 10)
+
+    end = datetime.datetime.today().strftime('%Y-%m-%d')
+    # end = datetime.datetime(2020, 7, 10)
+
+    Treasury = web.DataReader(['TB1YR'], 'fred', start, end)
+    RF = float(Treasury.iloc[-1])
+    RF = RF / 100
+    print(RF, interest_coverage_ratio)
+    return [RF, interest_coverage_ratio]
+
 
 def calculate_cost_of_debt(ticker):
-
     pass
+
 
 def calculate_cost_of_equity():
     pass
 
 
-#return wacc
+# return wacc
 def calculate_WACC():
     pass
 
 
-#A DCF valuation for mature dividend paying companies with 10% error marginal
-#Consider country codes for different perpetual growthrates
+# A DCF valuation for mature dividend paying companies with 10% error marginal
+# Consider country codes for different perpetual growthrates
 # pass all the extracted data for here as following:
 # present cashflows in array
 # cashflow growth rate
-#wacc
+# wacc
 
 def calculate_DCF():
     pass
 
+
 def calculate_graham_number():
     pass
-
-
-
-
